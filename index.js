cfb5055315aa71dc287c42c22774ca03539a9f70
Kia Neitzell <kia.neitzell@gmail.com>
2024-01-18 19:14:25 -0800

    compleated

---
+++ index.js
@@ -0,0 +1,38 @@
+function add(a, b) {
+    return a + b;
+}
+function increment(n){
+    ++n
+    return(n)
+}
+
+
+    function subtract(a, b) {
+return a - b;
+    }
+    function decrement(n){
+        --n
+        return(n)
+    }
+
+    function multiply(a, b) {
+        return a * b;
+    }
+ 
+    console.log(result);
+
+
+    function divide(a, b) {
+        return a / b;
+        
+    }
+
+    console.log(result);
+
+   function makeInt(n) {
+    return parseInt(n, 10)
+   }
+   function preserveDecimal(n) {
+    return parseFloat(n)
+ }
+ 
